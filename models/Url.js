1a0be9d6570ea3b6588941c46c45b652abef4d48
Vivek <viveksingh599999@gmail.com>
2026-02-28 22:33:35 +0530

    feat: add MongoDB database schema

---
+++ models/Url.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const urlSchema = new mongoose.Schema({
+    urlCode: {
+        type: String,
+        required: true,
+    },
+    longUrl: {
+        type: String,
+        required: true,
+    },
+    shortUrl: {
+        type: String,
+        required: true,
+    },
+    clicks: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model("Url", urlSchema);
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
+const connectDB = require("./config/db");
 const express = require("express");
+
+connectDB();
 const app = express();
+
 app.get("/", (req, res) => {
     res.send("API is running....");
 });
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server is available and running on port ${PORT}`));
