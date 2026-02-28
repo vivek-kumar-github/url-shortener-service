d5f02667fc5268fa7fe2a844c3bda9a077a8d08f
Vivek <viveksingh599999@gmail.com>
2026-02-28 15:16:35 +0530

    Initial commit after setting up Express server

---
+++ server.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+    res.send("API is running....");
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is available and running on port ${PORT}`));
