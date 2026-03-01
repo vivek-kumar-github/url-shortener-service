f035bfa3a3e7fe761065d7d860957693f6aae8fb
Vivek <viveksingh599999@gmail.com>
2026-03-01 19:56:53 +0530

    feat: check for existing URL in database with mongoose

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,17 +1,16 @@
 const validUrl = require("valid-url");
+const Url = require("../models/Url");
 
 /**
  * @desc   This function will be responsible for creating new short URL.
- *         It will handle the business logic of validating the long URL,
- *         checking for its existence, generating a short code, and saving
- *         it to the database.
- * @routes POST /api/shorten
+ * @route  POST /api/shorten
  * @access Public
  */
 
 const shortenUrl = async (req, res) => {
     const { longUrl } = req.body;
     console.log("Recieved long URL: ", longUrl);
+
     if (!longUrl) {
         return res.status(400).json({ success: false, error: "Please provide a URL" });
     }
@@ -20,7 +19,19 @@ const shortenUrl = async (req, res) => {
         return res.status(400).json({ success: false, error: "Invalid URL format provided" });
     }
 
-    res.status(200).json({ success: true, message: "Controller is now connected!..." });
+    try {
+        let url = await Url.findOne({longUrl: longUrl });
+        if (url) {
+            return res.status(200).json({ success: true, data: url });
+        }
+
+        res.status(200).json({ success: true, message: "URL is new and valid."});
+        
+    } catch (err) {
+        console.error("Database error:", err);
+        
+        res.status(500).json({ success: false, error: "Internal Server Error" });
+    }
 };
 
 module.exports = {
