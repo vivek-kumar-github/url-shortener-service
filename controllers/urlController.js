65409375537bac4c9a7a5300df3e9b9bee66f202
Vivek <viveksingh599999@gmail.com>
2026-03-01 19:28:23 +0530

    feat: implement server-side URL validation

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,3 +1,5 @@
+const validUrl = require("valid-url");
+
 /**
  * @desc   This function will be responsible for creating new short URL.
  *         It will handle the business logic of validating the long URL,
@@ -8,9 +10,17 @@
  */
 
 const shortenUrl = async (req, res) => {
-    
+    const { longUrl } = req.body;
+    console.log("Recieved long URL: ", longUrl);
+    if (!longUrl) {
+        return res.status(400).json({ success: false, error: "Please provide a URL" });
+    }
 
-    res.status(200).json({ success: true, message: "Controller is now connected!..."});
+    if (!validUrl.isUri(longUrl)) {
+        return res.status(400).json({ success: false, error: "Invalid URL format provided" });
+    }
+
+    res.status(200).json({ success: true, message: "Controller is now connected!..." });
 };
 
 module.exports = {
