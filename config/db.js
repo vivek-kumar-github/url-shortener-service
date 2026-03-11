3a532e7e0007ced5cd7b48fb7e7eba9d4960c313
Vivek Kumar <viveksingh599999@gmail.com>
2026-03-11 14:31:25 +0530

    chore: prepare for deployment

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
+        console.log(error);
         process.exit(1);
     }
 };
